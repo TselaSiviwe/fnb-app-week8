9e1117f484023758e6a6b29195ebec58fe3e3d12
TselaSiviwe <tselaman01@gmail.com>
2025-07-04 19:41:41 +0530

    Add files via upload

---
+++ dictionary,py.py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "Alice",
+    "age": 25,
+    "city": "New York"
+}
+
+print(my_dict["name"])  
+
+my_dict["age"] = 26  
+my_dict["country"] = "USA"  
+print(my_dict)
---
+++ set.py
@@ -0,0 +1,8 @@
+my_set = {1, 2, 3, 2, 1}
+print(my_set)  
+
+my_set.add(4)
+print(my_set)  
+
+my_set.remove(2)
+print(my_set)  
